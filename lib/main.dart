6904eb1b11afd5a760a92c16186e4f0d94967179
Aatwan <153384512+Aatwan@users.noreply.github.com>
2024-05-17 23:54:07 +0300

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app/weather_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Weather App',
+      home: WeatherScreen(),
+    );
+  }
+}
